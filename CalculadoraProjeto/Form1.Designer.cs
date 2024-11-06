8a71e66f54287323e37f3f40f4bccfddd8fed738
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-06 17:15:54 -0300

    Postando o projeto

---
+++ CalculadoraProjeto/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿using System;
+
+namespace CalculadoraProjeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texVisor = new System.Windows.Forms.TextBox();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn_divisao = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn_multiplicacao = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn_subtracao = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn_adicao = new System.Windows.Forms.Button();
+            this.btn_igual = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // texVisor
+            // 
+            this.texVisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.texVisor.Location = new System.Drawing.Point(224, 70);
+            this.texVisor.Name = "texVisor";
+            this.texVisor.Size = new System.Drawing.Size(365, 38);
+            this.texVisor.TabIndex = 0;
+            this.texVisor.Text = "0";
+            this.texVisor.TextChanged += new System.EventHandler(this.texVisor_TextChanged);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(224, 114);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(71, 53);
+            this.btn7.TabIndex = 1;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn_divisao
+            // 
+            this.btn_divisao.Location = new System.Drawing.Point(518, 114);
+            this.btn_divisao.Name = "btn_divisao";
+            this.btn_divisao.Size = new System.Drawing.Size(71, 53);
+            this.btn_divisao.TabIndex = 2;
+            this.btn_divisao.Text = "/";
+            this.btn_divisao.UseVisualStyleBackColor = true;
+            this.btn_divisao.Click += new System.EventHandler(this.btn_divisao_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(318, 114);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(71, 53);
+            this.btn8.TabIndex = 3;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(418, 114);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(71, 53);
+            this.btn9.TabIndex = 4;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(418, 173);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(71, 53);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(318, 173);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(71, 53);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn_multiplicacao
+            // 
+            this.btn_multiplicacao.Location = new System.Drawing.Point(518, 173);
+            this.btn_multiplicacao.Name = "btn_multiplicacao";
+            this.btn_multiplicacao.Size = new System.Drawing.Size(71, 53);
+            this.btn_multiplicacao.TabIndex = 6;
+            this.btn_multiplicacao.Text = "*";
+            this.btn_multiplicacao.UseVisualStyleBackColor = true;
+            this.btn_multiplicacao.Click += new System.EventHandler(this.btn_multiplicacao_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(224, 173);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(71, 53);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(418, 232);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(71, 53);
+            this.btn3.TabIndex = 12;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(318, 232);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(71, 53);
+            this.btn2.TabIndex = 11;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn_subtracao
+            // 
+            this.btn_subtracao.Location = new System.Drawing.Point(518, 232);
+            this.btn_subtracao.Name = "btn_subtracao";
+            this.btn_subtracao.Size = new System.Drawing.Size(71, 53);
+            this.btn_subtracao.TabIndex = 10;
+            this.btn_subtracao.Text = "-";
+            this.btn_subtracao.UseVisualStyleBackColor = true;
+            this.btn_subtracao.Click += new System.EventHandler(this.btn_subtracao_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(224, 232);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(71, 53);
+            this.btn1.TabIndex = 9;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn_adicao
+            // 
+            this.btn_adicao.Location = new System.Drawing.Point(418, 291);
+            this.btn_adicao.Name = "btn_adicao";
+            this.btn_adicao.Size = new System.Drawing.Size(71, 53);
+            this.btn_adicao.TabIndex = 16;
+            this.btn_adicao.Text = "+";
+            this.btn_adicao.UseVisualStyleBackColor = true;
+            this.btn_adicao.Click += new System.EventHandler(this.btn_adicao_Click);
+            // 
+            // btn_igual
+            // 
+            this.btn_igual.Location = new System.Drawing.Point(318, 291);
+            this.btn_igual.Name = "btn_igual";
+            this.btn_igual.Size = new System.Drawing.Size(71, 53);
+            this.btn_igual.TabIndex = 15;
+            this.btn_igual.Text = "=";
+            this.btn_igual.UseVisualStyleBackColor = true;
+            this.btn_igual.Click += new System.EventHandler(this.btn_igual_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(224, 291);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(71, 53);
+            this.btn_limpar.TabIndex = 14;
+            this.btn_limpar.Text = "C";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(518, 296);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(71, 43);
+            this.btn0.TabIndex = 17;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn_adicao);
+            this.Controls.Add(this.btn_igual);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn_subtracao);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn_multiplicacao);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn_divisao);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.texVisor);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.TextBox texVisor;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn_divisao;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn_multiplicacao;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn_subtracao;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn_adicao;
+        private System.Windows.Forms.Button btn_igual;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn0;
+    }
+}
---
+++ CalculadoraProjeto/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculadoraProjeto
+{
+    public partial class Form1 : Form
+    {
+        // Variáveis para armazenar os números e a operação selecionada
+        private double primeiroNumero = 0; // Armazena o primeiro número
+        private double segundoNumero = 0;   // Armazena o segundo número
+        private string operacao = "";        // Armazena a operação selecionada
+
+        public Form1()
+        {
+            InitializeComponent(); // Inicializa os componentes da interface
+        }
+
+        private void texVisor_TextChanged(object sender, EventArgs e)
+        {
+            // Evento chamado quando o texto no visor muda (não precisa de implementação aqui)
+        }
+
+        // Função para adicionar dígitos ao visor
+        private void AdicionarDigito(string digito)
+        {
+            // Se o visor contém "0", limpa o visor antes de adicionar um novo dígito
+            if (texVisor.Text == "0")
+            {
+                texVisor.Clear(); // Limpa o visor
+            }
+            texVisor.Text += digito; // Adiciona o dígito ao visor
+        }
+
+        // Métodos para cada botão numérico (0 a 9)
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("0"); // Chama a função para adicionar o dígito 0
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("1"); // Chama a função para adicionar o dígito 1
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("2"); // Chama a função para adicionar o dígito 2
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("3"); // Chama a função para adicionar o dígito 3
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("4"); // Chama a função para adicionar o dígito 4
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("5"); // Chama a função para adicionar o dígito 5
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("6"); // Chama a função para adicionar o dígito 6
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("7"); // Chama a função para adicionar o dígito 7
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("8"); // Chama a função para adicionar o dígito 8
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            AdicionarDigito("9"); // Chama a função para adicionar o dígito 9
+        }
+
+        // Método para adicionar a operação de adição
+        private void btn_adicao_Click(object sender, EventArgs e)
+        {
+            // Tenta converter o texto do visor em um número
+            if (double.TryParse(texVisor.Text, out primeiroNumero))
+            {
+                operacao = "+"; // Define a operação como adição
+                texVisor.Text += " + "; // Adiciona a operação ao visor
+            }
+            else
+            {
+                // Mensagem de erro se o valor no visor não for um número válido
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        // Método para adicionar a operação de subtração
+        private void btn_subtracao_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(texVisor.Text, out primeiroNumero))
+            {
+                operacao = "-"; // Define a operação como subtração
+                texVisor.Text += " - "; // Adiciona a operação ao visor
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        // Método para adicionar a operação de multiplicação
+        private void btn_multiplicacao_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(texVisor.Text, out primeiroNumero))
+            {
+                operacao = "*"; // Define a operação como multiplicação
+                texVisor.Text += " * "; // Adiciona a operação ao visor
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        // Método para adicionar a operação de divisão
+        private void btn_divisao_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(texVisor.Text, out primeiroNumero))
+            {
+                operacao = "/"; // Define a operação como divisão
+                texVisor.Text += " / "; // Adiciona a operação ao visor
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        // Método para calcular o resultado quando o botão igual é pressionado
+        private void btn_igual_Click(object sender, EventArgs e)
+        {
+            // Divide o texto do visor em partes, usando espaços como delimitadores
+            string[] partes = texVisor.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Verifica se a entrada tem o formato correto
+            if (partes.Length == 3 && double.TryParse(partes[0], out primeiroNumero) && double.TryParse(partes[2], out segundoNumero))
+            {
+                double resultado = 0; // Variável para armazenar o resultado
+
+                // Calcula o resultado com base na operação selecionada
+                switch (partes[1])
+                {
+                    case "+":
+                        resultado = primeiroNumero + segundoNumero; // Adição
+                        break;
+                    case "-":
+                        resultado = primeiroNumero - segundoNumero; // Subtração
+                        break;
+                    case "*":
+                        resultado = primeiroNumero * segundoNumero; // Multiplicação
+                        break;
+                    case "/":
+                        // Verifica se o segundo número não é zero antes de dividir
+                        if (segundoNumero != 0)
+                        {
+                            resultado = primeiroNumero / segundoNumero; // Divisão
+                        }
+                        else
+                        {
+                            MessageBox.Show("Divisão por zero não é permitida."); // Mensagem de erro
+                            return;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                texVisor.Text = resultado.ToString(); // Exibe o resultado no visor
+            }
+            else
+            {
+                // Mensagem de erro se o formato da operação for inválido
+                MessageBox.Show("Formato da operação inválido.");
+            }
+        }
+
+        // Método para limpar o visor e resetar os valores
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            texVisor.Clear(); // Limpa o visor
+            primeiroNumero = 0; // Reseta o primeiro número
+            segundoNumero = 0; // Reseta o segundo número
+            operacao = ""; // Reseta a operação
+        }
+    }
+}
